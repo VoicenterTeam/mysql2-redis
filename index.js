673887fb90a09fd41f9bb3b2afdaa4b9c26c4232
Victoria_Scherba <victoria.sch@voicenter.com>
2021-06-24 15:02:26 +0300

    Change mysql2-redis

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 const crypto = require('crypto');
 
-const HashTypes = {
-  md5: 5,
-};
-
-const Caching = {
-  CACHE: 0,
-  SKIP: 1,
-  REFRESH: 2,
-};
-
-Object.freeze(HashTypes);
-
-const defaultCacheOptions = {
-  expire: 2629746,
-  keyPrefix: 'sql.',
-  hashType: HashTypes.md5,
-  caching: Caching.CACHE,
-};
-
-const md5Hash = (sql) => crypto
-  .createHash('md5')
-  .update(sql)
-  .digest('base64');
+const md5Hash = (sql) => crypto.createHash('md5').update(sql).digest('base64');
 
 const hash = (sql) => md5Hash(sql);
 
@@ -37,181 +15,36 @@ const parseRedisResult = (redisResult, key) => {
 };
 
 class MysqlRedis {
-  constructor(mysqlConn, redisClient, cacheOptions) {
+  constructor(mysqlConn, redisClient) {
     this.mysqlConn = mysqlConn;
     this.redisClient = redisClient;
     this.cacheOptions = {
-      expire:
-                (cacheOptions && cacheOptions.expire) || defaultCacheOptions.expire,
-      keyPrefix:
-                (cacheOptions && cacheOptions.keyPrefix)
-                || defaultCacheOptions.keyPrefix,
-      hashType:
-                (cacheOptions && cacheOptions.hashType) || defaultCacheOptions.hashType,
-      caching:
-                (cacheOptions && cacheOptions.caching) || defaultCacheOptions.caching,
+      expire: 2629746,
+      keyPrefix: 'sql.',
+      hashType: 'md5',
+      caching: 0,
     };
   }
 
   query(sql, values, _options, _cb) {
-    const cb = _cb || (_options || values); // in case expire is not provided, cb is third arg
-    const options = _cb ? _options : !Array.isArray(values) ? values : {};
+    const cb = _cb || (_options || values);
+    const s = sql + JSON.stringify(values);
 
-    const _s = sql + JSON.stringify(values);
+    const hashType = this.cacheOptions;
 
-    const prefix = (options && options.keyPrefix) || this.cacheOptions.keyPrefix;
+    const key = hash(s, hashType);
 
-    const hashType = (options && options.hashType) || this.cacheOptions.hashType;
-
-    const key = prefix + ((options && options.hash) || hash(_s, hashType));
-
-    const caching = (options && options.caching) || this.cacheOptions.caching;
-    switch (caching) {
-      case Caching.CACHE:
-      default:
-        this.redisClient.get(key, (redisErr, redisResult) => {
-          if (redisErr || redisResult == null) {
-            this.mysqlConn.query(
-              sql,
-              Array.isArray(values) ? values : [],
-              (mysqlErr, mysqlResult, fields) => {
-                if (mysqlErr) {
-                  return cb(mysqlErr, null);
-                }
-                if (!redisErr) {
-                  this.redisClient.set(
-                    key,
-                    JSON.stringify(
-                      mysqlResult.length > 0 && Array.isArray(mysqlResult[0])
-                        ? [mysqlResult[0]]
-                        : mysqlResult,
-                    ),
-                    'EX',
-                    (options && options.expire) || this.cacheOptions.expire,
-                  );
-                }
-                return cb(mysqlErr, mysqlResult, fields);
-              },
-            );
-          } else {
-            return cb(null, parseRedisResult(redisResult, key));
-          }
-        });
-    }
-  }
-}
-
-// PROMISE API
-
-class MysqlRedisAsync {
-  constructor(mysqlConn, redisClient, cacheOptions) {
-    this.mysqlConn = mysqlConn;
-    this.redisClient = redisClient;
-    this.cacheOptions = {
-      expire:
-                (cacheOptions && cacheOptions.expire) || defaultCacheOptions.expire,
-      keyPrefix:
-                (cacheOptions && cacheOptions.keyPrefix)
-                || defaultCacheOptions.keyPrefix,
-      hashType:
-                (cacheOptions && cacheOptions.hashType) || defaultCacheOptions.hashType,
-      caching:
-                (cacheOptions && cacheOptions.caching) || defaultCacheOptions.caching,
-    };
-  }
-
-  query(sql, values, options) {
-    // cb = cb || options || values; //in case expire is not provided, cb is third arg
-
-    return new Promise(async (resolve, reject) => {
-      options = options || (!Array.isArray(values) ? values : null);
-
-      const _s = sql + JSON.stringify(values);
-      const prefix = (options && options.keyPrefix) || this.cacheOptions.keyPrefix;
-
-      const hashType = (options && options.hashType) || this.cacheOptions.hashType;
-
-      const key = prefix + ((options && options.hash) || hash(_s, hashType));
-
-      const caching = (options && options.caching) || this.cacheOptions.caching;
-
-      switch (caching) {
-        case Caching.SKIP:
-          try {
-            const [mysqlResult, fields] = await this.mysqlConn.query(
-              sql,
-              Array.isArray(values) ? values : [],
-            );
-
-            resolve([mysqlResult, fields]);
-          } catch (mysqlErr) {
-            reject(mysqlErr);
-          }
-
-          break;
-        case Caching.REFRESH:
-          try {
-            const [mysqlResult, fields] = await this.mysqlConn.query(
-              sql,
-              Array.isArray(values) ? values : [],
-            );
-            await this.redisClient.set(
-              key,
-              JSON.stringify(mysqlResult),
-              'EX',
-              (options && options.expire) || this.cacheOptions.expire,
-            );
-            resolve([mysqlResult, fields]);
-          } catch (mysqlErr) {
-            reject(mysqlErr);
-          }
-          break;
-
-        case Caching.CACHE:
-        default:
-          try {
-            const redisResult = await this.redisClient.get(key);
-
-            if (redisResult) {
-              resolve(parseRedisResult(redisResult, key));
-            } else {
-              try {
-                const [mysqlResult, fields] = await this.mysqlConn.query(
-                  sql,
-                  Array.isArray(values) ? values : [],
-                );
-                await this.redisClient.set(
-                  key,
-                  JSON.stringify(
-                    mysqlResult.length > 0 && Array.isArray(mysqlResult[0])
-                      ? [mysqlResult[0]]
-                      : mysqlResult,
-                  ),
-                  'EX',
-                  (options && options.expire) || this.cacheOptions.expire,
-                );
-                resolve([mysqlResult, fields]);
-              } catch (mysqlErr) {
-                reject(mysqlErr);
-              }
-            }
-          } catch (redisErr) {
-            try {
-              const [mysqlResult, fields] = await this.mysqlConn.query(
-                sql,
-                Array.isArray(values) ? values : [],
-              );
-              resolve([mysqlResult, fields]);
-            } catch (mysqlErr) {
-              reject(mysqlErr);
-            }
-          }
-          break;
+    this.redisClient.get(key, (redisErr, redisResult) => {
+      if (redisErr || redisResult == null) {
+        this.mysqlConn.query(sql,
+            Array.isArray(values) ? values : [],
+            (mysqlErr, mysqlResult, fields) => cb(mysqlErr, mysqlResult, fields));
+      } else {
+        return cb(null, parseRedisResult(redisResult, key));
       }
     });
   }
 }
-
 module.exports = {
-  MysqlRedis, MysqlRedisAsync, HashTypes, Caching,
+  MysqlRedis,
 };
