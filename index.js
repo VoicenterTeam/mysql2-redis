6566fe3fb7534bf7274c425f835a91b0e707a8ea
Victoria_Scherba <victoria.sch@voicenter.com>
2021-07-09 12:03:00 +0300

    Update index and Add debug

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const crypto = require('crypto');
 
-const checkValues = (values) => (Array.isArray(values) ? values : Array.from(values));
-
 const defaultCacheOptions = {
   keyPrefix: 'sql.',
   algorithm: 'md5',
   encoding: 'base64',
+  colorCode: '31',
+  debug: false,
 };
 
 class MysqlRedis {
@@ -13,6 +13,14 @@ class MysqlRedis {
     this.mysqlConn = mysqlConn;
     this.redisClient = redisClient;
     this.cacheOptions = { ...defaultCacheOptions, ...cacheOptions };
+  }
+
+  log(message) {
+    console.log(`[${new Date().toLocaleTimeString()}] \x1b[${this.cacheOptions.colorCode}mmysql2-redis\x1b[0m: ${message}`);
+  }
+
+  checkValues(values) {
+    return (Array.isArray(values) ? values : Array.from(values));
   }
 
   hash(sql) {
@@ -25,31 +33,35 @@ class MysqlRedis {
   checkMysqlResult(mysqlResult) {
     if (mysqlResult.length > 0 && Array.isArray(mysqlResult[0])) {
       return Array.from(mysqlResult[0]);
-    } return mysqlResult;
-  }
+    }
 
-  parseRedisResult(redisResult) {
-    const result = JSON.parse(redisResult);
-
-    return result;
+    return mysqlResult;
   }
 
   query(sql, values, callback) {
-    const selectSQL = sql + JSON.stringify(values);
-    const key = this.cacheOptions.keyPrefix + this.hash(selectSQL);
+    const query = sql + JSON.stringify(values);
+    const key = this.cacheOptions.keyPrefix + this.hash(query);
 
     this.redisClient.get(key, (redisErr, redisResult) => {
+      if (this.cacheOptions.debug) this.log(`searching for key ${key}`);
+
       if (redisErr || redisResult == null) {
-        this.mysqlConn.query(sql, checkValues(values), (mysqlErr, mysqlResult) => {
+        if (this.cacheOptions.debug) this.log(`key: ${key} not found`);
+
+        return this.mysqlConn.query(sql, this.checkValues(values), (mysqlErr, mysqlResult) => {
           const mysqlJSON = JSON.stringify(this.checkMysqlResult(mysqlResult));
 
+          if (this.cacheOptions.debug) this.log(`creating new key ${key}`);
+
           this.redisClient.set(key, mysqlJSON);
+
           return callback(mysqlErr, mysqlResult);
         });
-      } else {
-        return callback(null, this.parseRedisResult(redisResult));
       }
-      return '';
+
+      if (this.cacheOptions.debug) this.log(`key ${key} successfully found`);
+
+      return callback(null, JSON.parse(redisResult));
     });
   }
 }
